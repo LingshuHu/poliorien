e6112685b3bd5fcc13d65d43e3dd832064c2adbc
Lingshu Hu <lingshu.hu@hotmail.com>
2021-05-26 00:08:02 -0500

    update

--- R_scripts/get_test_user_data.R
+++ R_scripts/get_test_user_data.R
@@ -6,7 +6,7 @@ usr_id <- read.csv("Twitter_user_lists_test_deeplearning_Jan_10.csv",
 usr_id$ID <- sub("@", "", usr_id$ID)
 usr_id$ID <- sub("\\s+", "", usr_id$ID)
 
-rt <- search_tweets("covid", 18000)
+#rt <- search_tweets("covid", 18000)
 
 usr <- get_timeline(usr_id$ID, 
                     n = 3200, 
